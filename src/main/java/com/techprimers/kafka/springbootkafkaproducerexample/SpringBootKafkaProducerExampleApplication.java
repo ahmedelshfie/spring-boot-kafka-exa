19008e529b2341fa9d1a523d45d3da73c8398550
ahmedelshfie <ahmedelshfie@gmail.com>
2019-01-20 22:34:47 -0200

    test

--- src/main/java/com/techprimers/kafka/springbootkafkaproducerexample/SpringBootKafkaProducerExampleApplication.java
+++ src/main/java/com/techprimers/kafka/springbootkafkaproducerexample/SpringBootKafkaProducerExampleApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootKafkaProducerExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootKafkaProducerExampleApplication.class, args);
+		SpringApplication.run(SpringBootKafkaProducerExampleApplication.class, args); 
 	}
 }
